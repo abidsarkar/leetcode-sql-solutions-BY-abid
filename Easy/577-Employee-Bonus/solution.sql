5eceb4599235b3b9ab3fbad512bba1e8871243ad
abidsarkar <abidsarkar0@gmail.com>
2026-07-01 16:17:37 +0600

    left join

--- Easy/577-Employee-Bonus/solution.sql
+++ Easy/577-Employee-Bonus/solution.sql
@@ -11,3 +11,12 @@ Concepts:
 */
 
 -- Write your solution here
+-- Write your PostgreSQL query statement below
+select
+a.name,
+b.bonus
+from Employee a
+left JOIN Bonus b
+ON a.empId = b.empId
+where b.bonus < 1000
+or b.bonus is null
