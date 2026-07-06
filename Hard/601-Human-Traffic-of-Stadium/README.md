c6785a02212e57115700e6e71f64e2d193001579
abidsarkar <abidsarkar0@gmail.com>
2026-07-06 15:12:07 +0600

    row number and count

--- Hard/601-Human-Traffic-of-Stadium/README.md
+++ Hard/601-Human-Traffic-of-Stadium/README.md
@@ -15,6 +15,9 @@ https://leetcode.com/problems/human-traffic-of-stadium/
 - 
 
 ---
+so first we need to find out the id and other having the people over 100
+then next count if we do id-row_number over 100 then without gap it make same row difference
+then from the difference count how many in a row and print them
 
 ## Notes
 
--- Hard/601-Human-Traffic-of-Stadium/solution.sql
+++ Hard/601-Human-Traffic-of-Stadium/solution.sql
@@ -11,4 +11,28 @@ Concepts:
 */
 
 -- Write your SQL solution here
-
+with continous_group as(
+    select
+    id,
+    visit_date,
+    people,
+    id - row_number() over(order by id) as group_id
+    from Stadium
+    where people >=100
+),
+how_many_group as (
+    select
+    id,
+    visit_date,
+    people,
+    count(*) over(partition by group_id) as group_count
+    from continous_group
+)
+select 
+id,
+visit_date,
+people
+from how_many_group
+where group_count >=3
+order by visit_date asc
+ 
