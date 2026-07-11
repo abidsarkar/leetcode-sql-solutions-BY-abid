c63a13eecc3cb9e8a97a6fb221d151c3f498da02
abidsarkar <abidsarkar0@gmail.com>
2026-07-11 18:12:23 +0600

    alter id between odd and even using of case in sql

--- Medium/626-Exchange-Seats/solution.sql
+++ Medium/626-Exchange-Seats/solution.sql
@@ -12,3 +12,12 @@ Concepts:
 
 -- Write your SQL solution here
 
+select
+case 
+    when id % 2 =1 and id = (select max(id) from Seat) then id 
+    when id %2 = 1 then  id+1
+    else id -1
+end as id,
+student
+from Seat
+order by id asc
