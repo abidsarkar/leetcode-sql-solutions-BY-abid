238ce0fcd252eaedd41c2826cd1689d76234b88b
abidsarkar <abidsarkar0@gmail.com>
2026-07-17 22:14:20 +0600

    date diff and

--- Medium/1321-Restaurant-Growth/solution.sql
+++ Medium/1321-Restaurant-Growth/solution.sql
@@ -12,3 +12,41 @@ Concepts:
 
 -- Write your SQL solution here
 
+WITH
+  Dates AS (
+    SELECT DISTINCT visited_on
+    FROM Customer
+    WHERE visited_on >= (
+      SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+      FROM Customer
+    )
+  )
+SELECT
+  a.visited_on,
+  SUM(b.amount) AS amount,
+  ROUND(SUM(b.amount) / 7, 2) AS average_amount
+FROM Dates a
+LEFT JOIN Customer b
+  ON (DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6)
+GROUP BY 1;
+-- in postgresql
+SELECT 
+    visited_on,
+    amount,
+    ROUND(average_amount::numeric, 2) AS average_amount
+FROM (
+    SELECT 
+        visited_on,
+        SUM(SUM(amount)) OVER(
+            ORDER BY visited_on 
+            RANGE BETWEEN INTERVAL '6 days' PRECEDING AND CURRENT ROW
+        ) AS amount,
+        AVG(SUM(amount)) OVER(
+            ORDER BY visited_on 
+            RANGE BETWEEN INTERVAL '6 days' PRECEDING AND CURRENT ROW
+        ) AS average_amount,
+        ROW_NUMBER() OVER(ORDER BY visited_on) as row_num
+    FROM Customer
+    GROUP BY visited_on
+) t
+WHERE row_num >= 7;
--- Medium/1341-Movie-Rating/solution.sql
+++ Medium/1341-Movie-Rating/solution.sql
@@ -12,3 +12,46 @@ Concepts:
 
 -- Write your SQL solution here
 
+# Write your MySQL query statement below
+	(
+  SELECT b.name AS results
+  FROM MovieRating a
+   JOIN Users b
+    ON a.user_id = b.user_id
+  GROUP BY a.user_id
+  ORDER BY COUNT(a.movie_id) DESC, b.name
+  LIMIT 1
+)
+UNION ALL
+(
+  SELECT b.title AS results
+  FROM MovieRating a
+JOIN Movies b
+    ON a.movie_id = b.movie_id
+  WHERE DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+  GROUP BY a.movie_id
+  ORDER BY AVG(a.rating) DESC, b.title
+  LIMIT 1
+);
+-- Write your PostgreSQL query statement below
+
+	(
+  SELECT b.name AS results
+  FROM MovieRating a
+   JOIN Users b
+    ON a.user_id = b.user_id
+  GROUP BY a.user_id,b.name
+  ORDER BY COUNT(a.movie_id) DESC, b.name
+  LIMIT 1
+)
+UNION ALL
+(
+  SELECT b.title AS results
+  FROM MovieRating a
+JOIN Movies b
+    ON a.movie_id = b.movie_id
+  WHERE to_char(created_at, 'YYYY-MM') = '2020-02'
+  GROUP BY a.movie_id ,b.title
+  ORDER BY AVG(a.rating) DESC, b.title
+  LIMIT 1
+);
