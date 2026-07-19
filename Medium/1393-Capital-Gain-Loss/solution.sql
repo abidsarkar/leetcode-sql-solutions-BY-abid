f1c118ae63097a600ce0115c6f796d373ade5f76
abidsarkar <abidsarkar0@gmail.com>
2026-07-19 20:57:27 +0600

    filter in postgresql and case in mysql

--- Medium/1393-Capital-Gain-Loss/solution.sql
+++ Medium/1393-Capital-Gain-Loss/solution.sql
@@ -12,3 +12,14 @@ Concepts:
 
 -- Write your SQL solution here
 
+select
+stock_name,
+sum(price) filter(where operation ='Sell')-sum(price)filter(where operation  ='Buy') as capital_gain_loss 
+from Stocks
+group by 1
+# Write your MySQL query statement below
+select
+stock_name,
+sum(case when operation ='Sell'then price else 0 end) -sum(case when operation  ='Buy' then price else 0 end) as capital_gain_loss 
+from Stocks
+group by 1
