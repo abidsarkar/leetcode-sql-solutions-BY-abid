e143ff350260f6117bada85615785d0d4e504b24
abidsarkar <abidsarkar0@gmail.com>
2026-07-19 20:28:08 +0600

    left join

--- Easy/1378-Replace-Employee-ID-With-The-Unique-Identifier/solution.sql
+++ Easy/1378-Replace-Employee-ID-With-The-Unique-Identifier/solution.sql
@@ -11,3 +11,10 @@ Concepts:
 */
 
 -- Write your solution here
+-- Write your PostgreSQL query statement below
+select
+b.unique_id,
+a.name
+from Employees a
+LEFT JOIN EmployeeUNI b
+ON a.id = b.id
