45b547734ac04f6e0adfbf6fc8962dbc10250a8b
abidsarkar <abidsarkar0@gmail.com>
2026-07-21 23:47:01 +0600

    commit

--- Easy/1661-Average-Time-of-Process-per-Machine/solution.sql
+++ Easy/1661-Average-Time-of-Process-per-Machine/solution.sql
@@ -11,3 +11,17 @@ Concepts:
 */
 
 -- Write your solution here
+SELECT
+  a.machine_id,
+  ROUND(
+    AVG(b.timestamp - a.timestamp),
+    3
+  ) AS processing_time
+FROM Activity  a
+INNER JOIN Activity AS b
+  USING (machine_id, process_id)
+WHERE
+  a.activity_type = 'start'
+  AND b.activity_type = 'end'
+GROUP BY 1;
+
